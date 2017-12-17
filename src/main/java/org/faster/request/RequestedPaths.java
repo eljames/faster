1a04f14210e376dadbc88ea69eccdf8514e495a8
eljames <eljames4@hotmail.com>
2017-12-17 00:50:31 -0200

    Interface for get the PathItems iteration

---
+++ src/main/java/org/faster/request/RequestedPaths.java
@@ -0,0 +1,18 @@
+package org.faster.request;
+
+import java.io.IOException;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.faster.pathitems.PathItems;
+
+/**
+ * Request files and folders requested peer.
+ * @author Eli James Aguiar
+ *
+ */
+public interface RequestedPaths {
+	
+	PathItems request(CharSequence path) throws IOException, ProtocolSyntaxErrorException;
+	void close() throws IOException;
+
+}
