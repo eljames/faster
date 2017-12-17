f2367118918646b0ad0801698ade690d373fd270
eljames <eljames4@hotmail.com>
2017-12-17 00:26:43 -0200

    Exception related to protocol sintaxe error.

---
+++ src/main/java/org/faster/exception/ProtocolSyntaxErrorException.java
@@ -0,0 +1,19 @@
+package org.faster.exception;
+
+public class ProtocolSyntaxErrorException extends Exception {
+
+	public static String defaultMsg = "Procotol syntax error.";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ProtocolSyntaxErrorException() {
+		super(defaultMsg);
+	}
+	
+	public ProtocolSyntaxErrorException(String msg) {
+		super(defaultMsg);
+	}
+
+}
