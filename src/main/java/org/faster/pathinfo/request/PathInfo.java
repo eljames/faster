667e8bd70f528bc6ea33a7e3e76a8ec8edbda29e
eljames <eljames4@hotmail.com>
2017-12-17 00:46:16 -0200

    Path information interface

---
+++ src/main/java/org/faster/pathinfo/request/PathInfo.java
@@ -0,0 +1,26 @@
+package org.faster.pathinfo.request;
+
+public interface PathInfo {
+	
+	
+	/**
+	 * Returns true if directory, otherwise, a regular file.
+	 * @return 
+	 */
+	boolean isDirectory();
+	
+	/**
+	 * Returns a file path or directory path. The returned path is relative to the requested peer root '/'.
+	 * @return
+	 */
+	CharSequence path();
+	
+	
+	long size();
+	
+	/**
+	 * A object that represents a unknown path.
+	 */
+	public static final PathInfo NO_PATH_INFO = new NoPathInfo();
+
+}
