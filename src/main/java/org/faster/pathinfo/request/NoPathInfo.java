c7b0969ec3a87e78122ab8c4d16be8904b1dd3d6
eljames <eljames4@hotmail.com>
2017-12-17 00:45:43 -0200

    NoPathInfo is a PathInfo type for non returned path

---
+++ src/main/java/org/faster/pathinfo/request/NoPathInfo.java
@@ -0,0 +1,29 @@
+package org.faster.pathinfo.request;
+
+/**
+ * This type indicates that the path information does not exist.
+ * @author Eli James Aguiar
+ *
+ */
+public class NoPathInfo implements PathInfo {
+
+	@Override
+	public boolean isDirectory() {
+		throw exception();
+	}
+
+	@Override
+	public CharSequence path() {
+		throw exception();
+	}
+
+	@Override
+	public long size() {
+		throw exception();
+	}
+	
+	private UnsupportedOperationException exception() {
+		return new UnsupportedOperationException("Not supported for '" + this.getClass().getSimpleName() + "' class.");
+	}
+
+}
