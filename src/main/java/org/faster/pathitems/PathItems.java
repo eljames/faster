4cf1042f7995e3ff1e690d7c9928cbb12f870a8c
eljames <eljames4@hotmail.com>
2017-12-17 00:48:14 -0200

    Iteration for all files and directory from a directory.

---
+++ src/main/java/org/faster/pathitems/PathItems.java
@@ -0,0 +1,29 @@
+package org.faster.pathitems;
+
+import java.io.IOException;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.faster.pathinfo.request.PathInfo;
+
+/**
+ * Iteration on all files and directories from requested directory. Sub-directories not included.
+ * @author Eli James Aguiar
+ *
+ */
+public interface PathItems {
+	
+	/**
+	 * Move to the next path item.
+	 * @return true, if it has next item, false otherwise.
+	 * @throws IOException
+	 * @throws ProtocolSyntaxErrorException
+	 */
+	boolean next() throws IOException, ProtocolSyntaxErrorException;
+	
+	/**
+	 * Get the current path.
+	 * @return
+	 */
+	PathInfo path();
+
+}
