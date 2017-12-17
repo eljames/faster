bbb68549ac59ab8857940f7a9e7cad60c833f7e1
eljames <eljames4@hotmail.com>
2017-12-17 00:52:28 -0200

    Decorator for RequestedPaths

---
+++ src/main/java/org/faster/request/RqRequestedPaths.java
@@ -0,0 +1,52 @@
+package org.faster.request;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.faster.pathitems.PathItems;
+
+public class RqRequestedPaths implements RequestedPaths {
+	
+	private final RequestedPaths req;
+	private final OutputStream out;
+	
+	private static final String PATH_ITEMS = "pi";
+	private static final String LINEBREAK = "\n";
+
+	public RqRequestedPaths(RequestedPaths requestedPaths, OutputStream outputStream) {
+		this.req = requestedPaths;
+		this.out = outputStream;
+	}
+
+	@Override
+	public PathItems request(CharSequence path) throws IOException, ProtocolSyntaxErrorException {
+		
+		doRequest(
+			new BufferedWriter(
+				new OutputStreamWriter(this.out, StandardCharsets.UTF_8)
+			),
+			path
+		);
+
+		return req.request(path);
+		
+	}
+
+	@Override
+	public void close() throws IOException {
+		req.close();
+	}
+	
+	private void doRequest(Writer writer, CharSequence path) throws IOException {
+		writer.write(PATH_ITEMS);
+		writer.write(LINEBREAK);
+		writer.write(path.toString());
+		writer.flush();
+	}
+
+}
---
+++ src/main/java/org/faster/request/RqResponsedPaths.java
@@ -0,0 +1,47 @@
+package org.faster.request;
+
+import java.io.IOException;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.faster.pathitems.DfPathItems;
+import org.faster.pathitems.PathItems;
+import org.faster.token.TokenPathInfo;
+
+
+/**
+ * Decorator. Response requested peer.
+ * @author Eli James Aguiar
+ *
+ */
+public class RqResponsedPaths implements RequestedPaths {
+	
+	private final TokenPathInfo token;
+
+	public RqResponsedPaths(TokenPathInfo token) {
+		this.token = token;
+	}
+
+	@Override
+	public PathItems request(CharSequence path) throws IOException, ProtocolSyntaxErrorException {
+		
+		if(tokenOk()) {
+			return new DfPathItems(token);
+		}
+		
+		throw new ProtocolSyntaxErrorException("The server has not been responded with ok.");
+	}
+
+	@Override
+	public void close() throws IOException {
+		token.close();
+	}
+	
+	private boolean tokenOk() {
+		return this.token.next().equals(TokenKey.OK);
+	}
+	
+	private class TokenKey {
+		private static final String OK = "k";
+	}
+
+}
