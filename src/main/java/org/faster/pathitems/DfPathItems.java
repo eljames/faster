b934544e0d6ff3345b1d831c02c832348afd670b
eljames <eljames4@hotmail.com>
2017-12-17 00:39:09 -0200

    Default path items

---
+++ src/main/java/org/faster/pathitems/DfPathItems.java
@@ -0,0 +1,82 @@
+package org.faster.pathitems;
+
+import java.io.IOException;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.faster.pathinfo.request.PathInfo;
+import org.faster.pathinfo.request.PtPath;
+import org.faster.pathinfo.request.PtSize;
+import org.faster.pathinfo.request.PtType;
+import org.faster.token.TokenPathInfo;
+
+/**
+ * Decorator for 'Separator' token verification.
+ * The serator token is what divide between each path information.
+ * @author Eli James Aguiar
+ *
+ */
+public class DfPathItems implements PathItems {
+	
+	private final TokenPathInfo token;
+	private final Cache<Boolean> next;
+	private final Cache<PathInfo> path;
+	
+	private static final String END_TOKEN = "e";
+	
+	public DfPathItems(final TokenPathInfo tokenPath) {
+		this.token = tokenPath;
+		this.next = new Cache<Boolean>(true);
+		this.path = new Cache<PathInfo>(PathInfo.NO_PATH_INFO);
+	}
+
+	@Override
+	public boolean next() throws IOException, ProtocolSyntaxErrorException {
+		
+		if(this.next.element()) {
+			
+			String nextToken = this.token.next();
+			
+			if(hasNext(nextToken))
+				return true;
+			else if(nextToken.equals(END_TOKEN))
+				return this.next.element(false);
+			else {
+				this.next.element(false);
+				throw new ProtocolSyntaxErrorException("Expected token '' (empty token) or '" + END_TOKEN + "' (end token)");
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public PathInfo path() {
+		
+		if(this.next.element())
+			return this.path.element();
+		
+		return PathInfo.NO_PATH_INFO;
+	}
+	
+	private boolean hasNext(String nextToken) throws IOException, ProtocolSyntaxErrorException {
+		
+		// If token is empty string, there is next item;
+		if( ! nextToken.equals(""))
+			return false;
+			
+		nextElement();
+		return true;
+	}
+	
+	private void nextElement() throws IOException, ProtocolSyntaxErrorException {
+
+		this.path.element(
+			new PtSize(
+				new PtPath(
+					new PtType(
+						this.token),
+					this.token),
+				this.token
+			)
+		);
+	}
+}
