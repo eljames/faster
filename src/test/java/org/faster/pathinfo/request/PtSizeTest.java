8d7d4278585d4117c2c16413464ff8ae085de3ec
eljames <eljames4@hotmail.com>
2017-12-17 01:48:34 -0200

    PtSize tests created

--- src/test/java/org/faster/pathinfo/request/PtSizeTest.java
+++ src/test/java/org/faster/pathinfo/request/PtSizeTest.java
@@ -1,7 +1,50 @@
 package org.faster.pathinfo.request;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.junit.Test;
+
 public class PtSizeTest {
 
 	
+	@Test
+	public void returnsSize() throws IOException, ProtocolSyntaxErrorException {
+		
+		
+		long size = 1024;
+		String sizeStream = ""+size;
+		
+		PathInfo pathInfo = new PtSize(
+			PathInfo.NO_PATH_INFO,
+			new FakeTokenPathInfo(sizeStream + "\n")
+		);
+		
+		assertEquals(pathInfo.size(), size);
+	}
+	
+	@Test(expected = ProtocolSyntaxErrorException.class)
+	public void throwsIfNotNumber() throws IOException, ProtocolSyntaxErrorException {
+		
+		String sizeStream = "notNumber\n";
+		
+		new PtSize(
+			PathInfo.NO_PATH_INFO,
+			new FakeTokenPathInfo(sizeStream + "\n")
+		);
+	}
+	
+	@Test(expected = ProtocolSyntaxErrorException.class)
+	public void throwsIfPartialNumber() throws IOException, ProtocolSyntaxErrorException {
+		
+		String sizeStream = "1234b\n";
+		
+		new PtSize(
+			PathInfo.NO_PATH_INFO,
+			new FakeTokenPathInfo(sizeStream + "\n")
+		);
+	}
 	
 }
