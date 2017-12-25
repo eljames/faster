5b7a490a3a85b6b614ab9a1764c41e55092db8cd
eljames <eljames4@hotmail.com>
2017-12-24 22:32:25 -0200

    RpRequestTest added

---
+++ src/test/java/org/faster/request/RpRequestTest.java
@@ -0,0 +1,37 @@
+package org.faster.request;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.faster.exception.ProtocolSyntaxErrorException;
+import org.faster.pathitems.PathItems;
+import org.junit.Test;
+
+public class RpRequestTest {
+	
+	@Test
+	public void returnsRequest() throws IOException, ProtocolSyntaxErrorException {
+		
+		String path = "/abc/music";
+		String resp = "pi\n" + path;
+		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		new RpRequest(
+			new RequestedPaths() {
+				
+				@Override
+				public PathItems request(CharSequence path) throws IOException, ProtocolSyntaxErrorException {
+					return null;
+				}
+			},
+			out
+		).request(path);
+		
+		assertTrue(out.toString(StandardCharsets.UTF_8.name()).equals(resp));
+	}
+
+}
