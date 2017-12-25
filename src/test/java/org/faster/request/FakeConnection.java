2351cac7cb4d1185adddea689d6bc65f6286656a
eljames <eljames4@hotmail.com>
2017-12-25 02:45:33 -0200

    Fake Class Connection created for tests

---
+++ src/test/java/org/faster/request/FakeConnection.java
@@ -0,0 +1,36 @@
+package org.faster.request;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.faster.connection.Connection;
+
+public class FakeConnection implements Connection {
+	
+	private final OutputStream out;
+	private final InputStream in;
+	
+	
+
+	public FakeConnection(OutputStream out, InputStream in) {
+		this.out = out;
+		this.in = in;
+	}
+
+	@Override
+	public void close() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public OutputStream output() throws IOException {
+		return this.out;
+	}
+
+	@Override
+	public InputStream input() throws IOException {
+		return this.in;
+	}
+
+}
