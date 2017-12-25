69f12037ddc2b1877112427dbda5688a8501dc5d
eljames <eljames4@hotmail.com>
2017-12-24 22:29:43 -0200

    The socket connection is now delegated by Connection class

--- src/main/java/org/faster/request/DfRequestedPaths.java
+++ src/main/java/org/faster/request/DfRequestedPaths.java
@@ -3,34 +3,33 @@ package org.faster.request;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+import org.faster.connection.Connection;
 import org.faster.exception.ProtocolSyntaxErrorException;
 import org.faster.pathitems.PathItems;
 import org.faster.token.DfTokenPathInfo;
 
 public class DfRequestedPaths implements RequestedPaths {
 	
-	
-	private final Socket socket;
+	private final Connection con;
 	private final RequestedPaths requested;
 	private static final int BUFFER = 4096;
 	
-	public DfRequestedPaths(final String ipAddress, final int port) throws UnknownHostException, IOException {
+	public DfRequestedPaths(Connection connection) throws UnknownHostException, IOException {
 		
-		this.socket = new Socket(ipAddress, port);
+		this.con = connection;
 		
 		this.requested = new RpRequest(
 			new RpResponse(
 				new DfTokenPathInfo(
 					new Scanner(
-						new BufferedInputStream(socket.getInputStream(), BUFFER)
+						new BufferedInputStream(this.con.input(), BUFFER)
 					)
 				)
 			),
-			new BufferedOutputStream(socket.getOutputStream(), BUFFER)
+			new BufferedOutputStream(this.con.output(), BUFFER)
 		);
 		
 	}
@@ -38,11 +37,5 @@ public class DfRequestedPaths implements RequestedPaths {
 	@Override
 	public PathItems request(CharSequence path) throws IOException, ProtocolSyntaxErrorException {
 		return this.requested.request(path);
-	}
-
-	@Override
-	public void close() throws IOException {
-		this.requested.close();
-		this.socket.close();
 	}
 }
