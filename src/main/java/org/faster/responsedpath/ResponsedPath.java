34615947e8d6542c1beee3f6ba40885ff96d3efa
eljames <eljames4@hotmail.com>
2018-01-25 21:33:39 -0200

    Changed comments

--- src/main/java/org/faster/responsedpath/ResponsedPath.java
+++ src/main/java/org/faster/responsedpath/ResponsedPath.java
@@ -9,8 +9,8 @@ import java.nio.file.Path;
 public interface ResponsedPath {
 	
 	/**
-	 * Responde with path's collection according to the requested path.
-	 * @param relativePath
+	 * Respond with path's collection according to the requested path. The path must be a directory.
+	 * @param relativePath The relative path sent by requester. This path must be a directory.
 	 */
 
 	void respond(Path relativePath) throws IOException;
