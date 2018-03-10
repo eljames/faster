56f7727729ed1e3877c770b4d6b98b47aa00e0f7
eljames <eljames4@hotmail.com>
2018-03-10 11:04:15 -0300

    DirMap fake for tests.

--- src/test/java/org/faster/responsepaths/DmFake.java
+++ src/test/java/org/faster/responsepaths/DmFake.java
@@ -1,26 +1,46 @@
 package org.faster.responsepaths;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.faster.dirmap.DirMap;
 import org.faster.pathinfo.PathInfo;
+import org.faster.pathinfo.response.PtResponse;
 
 public class DmFake implements DirMap {
 	
-	private final boolean dir;
+	private final String root;
+	private final boolean has;
 	
-	public DmFake(final boolean dir) {
-		this.dir = dir;
+	public DmFake(final String path) {
+		this.root = path;
+		this.has = true;
 	}
-
+	
+	public DmFake(final boolean has) {
+		this.has = has;
+		this.root = "";
+	}
+	
 	@Override
 	public boolean has(CharSequence path) {
-		return dir;
+		return this.has;
 	}
 
 	@Override
 	public Collection<PathInfo> paths(String path) {
-		return null;
+		Path dirPath = Paths.get(new ResourcePath().get(this.getClass()) + this.root);
+		File[] paths = dirPath.resolve(Paths.get("/").relativize(Paths.get(path))).toFile().listFiles();
+		List<PathInfo> pathInfoList = new ArrayList<PathInfo>();
+		for(File file : paths) {
+			pathInfoList.add(new PtResponse(dirPath, file));
+		}
+		return pathInfoList;
 	}
+
 
 }
