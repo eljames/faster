a31e9ef406fbf646193c02b212f1da10ea548599
eljames <eljames4@hotmail.com>
2018-03-10 11:11:32 -0300

    Using File object instead of static method from java.nio

--- src/main/java/org/faster/pathinfo/response/PtResponse.java
+++ src/main/java/org/faster/pathinfo/response/PtResponse.java
@@ -1,36 +1,36 @@
 package org.faster.pathinfo.response;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import org.faster.pathinfo.PathInfo;
 
 public class PtResponse implements PathInfo {
 	
 	private final Path rootPath;
-	private final Path filePath;
+	private final File file;
 	
-	public PtResponse(final Path rootPath, final Path filePath) {
+	public PtResponse(final Path rootPath, final File filePath) {
 		this.rootPath = rootPath;
-		this.filePath = filePath;
+		this.file = filePath;
 	}
 
 	@Override
 	public boolean isDirectory() {
-		return Files.isDirectory(filePath);
+		return this.file.isDirectory();
 	}
 
 	@Override
 	public CharSequence path() {
 		return "/" + this.rootPath
-				.relativize(this.filePath)
+				.relativize(this.file.toPath())
 				.toString();
 	}
 
 	@Override
 	public long size() throws IOException {
 		if(isDirectory()) return -1;
-		return Files.size(this.filePath);
+		return this.file.length();
 	}
 
 }
