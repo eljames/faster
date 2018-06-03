0e8795b9281491da53757c0dc466695b7c080eb0
eljames <eljames4@hotmail.com>
2018-06-02 22:10:35 -0300

    Imports removed

--- src/test/java/org/faster/pathmap/PathMapTest.java
+++ src/test/java/org/faster/pathmap/PathMapTest.java
@@ -3,7 +3,6 @@ package org.faster.pathmap;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 
 import org.faster.path.TestPath;
