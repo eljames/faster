b266017733f621e84776b66e559c28471489c4ab
eljames <eljames4@hotmail.com>
2018-06-02 22:08:45 -0300

    Test for virtual root path

--- src/test/java/org/faster/pathmap/PathMapTest.java
+++ src/test/java/org/faster/pathmap/PathMapTest.java
@@ -1,12 +1,20 @@
 package org.faster.pathmap;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+
+import org.faster.path.TestPath;
 import org.faster.responsepaths.CreatedPathMap;
 import org.faster.responsepaths.ResourcePath;
 import org.faster.responsepaths.ResponsePaths;
@@ -30,6 +38,26 @@ public class PathMapTest {
 		assertTrue(list.get(0).path().equals("/media/file_test.txt"));
 		assertTrue(list.get(1).path().equals("/media/me.txt"));
 		assertTrue(list.get(2).path().equals("/media/travel"));
+	}
+	
+	
+	@Test
+	public void listRoot() throws IOException {
+		String resourcePath = new TestPath().resources();
+		
+		HashMap<String, Path> map = new HashMap<>();
+		map.put("/docs", Paths.get(resourcePath + "/org/faster/pathinfo/response"));
+		map.put("/photos", Paths.get(resourcePath + "org/faster/responsepaths/root/pics"));
+		
+		PathMap pathmap = new PmNoRelative(
+			new PmDefault(map)
+		);
+		VirtualPath path = pathmap.get("/");
+		List<VirtualPath> paths = new ArrayList<>(path.paths());
+		Collections.sort(paths, new VirtualComparator());
+		assertEquals(paths.get(0).path().toString(), "/docs");
+		assertEquals(paths.get(1).path().toString(), "/photos");
+		
 	}
 	
 	class VirtualComparator implements Comparator<VirtualPath> {
