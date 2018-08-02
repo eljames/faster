abceb1f8ffc0d04eb854e2ffe630edcdbb5b1ef6
eljames <eljames4@hotmail.com>
2018-08-01 22:57:41 -0300

    Error handled test when directory not found

--- src/test/java/org/faster/request/RpDefaultTest.java
+++ src/test/java/org/faster/request/RpDefaultTest.java
@@ -1,6 +1,7 @@
 package org.faster.request;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -8,10 +9,15 @@ import java.io.IOException;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 
+import org.faster.connection.Connection;
+import org.faster.errors.Errors;
+import org.faster.errors.ErrorsDefault;
+import org.faster.errors.HandledError;
 import org.faster.exception.ProtocolSyntaxErrorException;
 import org.faster.pathinfo.request.PathItemsToken;
 import org.faster.pathitems.PathItems;
 import org.faster.requestedpath.RpDefault;
+import org.faster.requestedpath.errors.DirectoryNotFound;
 import org.faster.requestedpath.RequestedPaths;
 import org.junit.Test;
 
@@ -42,6 +48,44 @@ public class RpDefaultTest {
 			n++;
 		}
 		assertEquals(4, n);
+	}
+	
+	@Test
+	public void handleErrorWhenFileNotFound() throws UnknownHostException, IOException, ProtocolSyntaxErrorException {
+		Connection con = new FakeConnection(new ByteArrayOutputStream(), new ByteArrayInputStream(
+				(
+					"err\n"
+				  + "dnf"
+				).getBytes(StandardCharsets.UTF_8)
+			)
+		);
+		HandledErrorTesting handled = new HandledErrorTesting();
+		Errors errors = new ErrorsDefault()
+		.add(new DirectoryNotFound(handled));
+		RequestedPaths req = new RpDefault(con, errors);
+		try {
+			req.request("/aaa");
+		} catch (Exception e) {}
+		assertTrue(handled.pass());
+	}
+	
+	static class HandledErrorTesting implements HandledError {
+		
+		private boolean pass;
+		
+		public HandledErrorTesting() {
+			this.pass = false;
+		}
+
+		@Override
+		public void handle(CharSequence error, CharSequence path) {
+			this.pass = true;
+		}
+		
+		public boolean pass() {
+			return this.pass;
+		}
+		
 	}
 
 }
