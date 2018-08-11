b1a41eeea36c2167119d4167cc688bd9844cde40
eljames <eljames4@hotmail.com>
2018-08-10 21:59:27 -0300

    Support directory size

--- src/test/java/org/faster/pathinfo/request/PathInfoToken.java
+++ src/test/java/org/faster/pathinfo/request/PathInfoToken.java
@@ -3,9 +3,13 @@ package org.faster.pathinfo.request;
 public class PathInfoToken {
 	
 	public String create(boolean directory, String path, long size) {
+		return this.create(directory, path, size, false);
+	}
+	
+	public String create(boolean directory, String path, long size, boolean folderSize) {
 		return (directory ? "d" : "f") + "\n" +
 				path +
-				(size != -1 ? "\n" + size : "");
+				(size != -1 || folderSize ? "\n" + size : "");
 	}
 
 }
