5c0ad7188db5221af5b8386347eb37b2c81769f3
eljames <eljames4@hotmail.com>
2018-08-14 02:23:50 -0300

    System.out.println removed.

--- src/test/java/org/faster/filedata/FdFileTest.java
+++ src/test/java/org/faster/filedata/FdFileTest.java
@@ -30,7 +30,6 @@ public class FdFileTest {
 		new FdFile(
 			token, byteIn, new FileDeliveredNotConsumed(delivered)
 		).download();
-		System.out.println(delivered.expected());
 		assertEquals(delivered.expected(), "this example must end here because this is a test.");
 	}
 	
