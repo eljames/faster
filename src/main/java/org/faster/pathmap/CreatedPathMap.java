c36aae55aa018949d22783532a583894875f68da
eljames <eljames4@hotmail.com>
2018-09-11 23:06:06 -0300

    CreatedPathMap for adding on easily way.

--- src/main/java/org/faster/pathmap/CreatedPathMap.java
+++ src/main/java/org/faster/pathmap/CreatedPathMap.java
@@ -1,13 +1,26 @@
 package org.faster.pathmap;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CreatedPathMap {
 	
-	public PathMap create(Map<String, Path> files) {
+	private final Map<String, Path> map;
+	
+	public CreatedPathMap() {
+		this.map = new HashMap<>();
+	}
+	
+	public CreatedPathMap add(String virtual, String real) {
+		this.map.put(virtual, Paths.get(real));
+		return this;
+	}
+	
+	public PathMap map() {
 		return new PmNoRelative(
-			new PmDefault(files)
+			new PmDefault(this.map)
 		);
 	}
 
