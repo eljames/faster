9d567fbc6edb81ab35a4879cd0e3f810b7003705
eljames <eljames4@hotmail.com>
2018-10-03 00:55:03 -0300

    Inversion Control of ServerSocket

--- src/main/java/org/faster/requestlistened/RlCreated.java
+++ src/main/java/org/faster/requestlistened/RlCreated.java
@@ -16,35 +16,31 @@ import org.faster.responselist.ResponseListDefault;
 public class RlCreated implements RequestListened {
 	
 	private final PathMap pathmap;
+	private final ServerSocket server;
 	private final FileFeedback feed;
-	private final int port;
 	
-	public RlCreated(final PathMap map, final FileFeedback fd, final int port) {
+	public RlCreated(final ServerSocket serv, final PathMap map, final FileFeedback fd) {
+		this.server = serv;
 		this.pathmap = map;
 		this.feed = fd;
-		this.port = port;
 	}
 	
-	public RlCreated(final PathMap map, final int port) {
-		this(map, FileFeedback.NOTHING, port);
+	public RlCreated(final ServerSocket serv, final PathMap map) {
+		this(serv, map, FileFeedback.NOTHING);
 	}
 
 	@Override
 	public void start() throws IOException {
 		ExecutorService executor = Executors.newFixedThreadPool(5);
 		ResponseList responses = responselist();
-		try(final ServerSocket server = new ServerSocket(this.port)) {
 			while(true) {
 				RequestListened request = new RlThread(
 					executor,
-					server.accept(),
+					this.server.accept(),
 					responses
 				);
 				request.start();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 	
 	private ResponseList responselist() {
