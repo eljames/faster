d6f9a8fb1a6b55e71ea61f8054445baa07bad8ea
eljames <eljames4@hotmail.com>
2018-10-03 00:54:23 -0300

    'remove' method added

--- src/main/java/org/faster/pathmap/CreatedPathMap.java
+++ src/main/java/org/faster/pathmap/CreatedPathMap.java
@@ -1,7 +1,9 @@
 package org.faster.pathmap;
 
+import java.io.IOException;
+
 public interface CreatedPathMap {
 	CreatedPathMap add(String virtual, String real);
-	CreatedPathMap remove(String virtual);
+	CreatedPathMap remove(String virtual) throws IOException;
 	PathMap map();
 }
