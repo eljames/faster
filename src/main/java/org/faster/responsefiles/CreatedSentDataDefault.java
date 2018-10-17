5be392eee1f0bbbc37b9abb25f56a5efcc722d6b
eljames <eljames4@hotmail.com>
2018-10-17 03:53:54 -0300

    Implementation returning a SentData.

--- src/main/java/org/faster/responsefiles/CreatedSentDataDefault.java
+++ src/main/java/org/faster/responsefiles/CreatedSentDataDefault.java
@@ -2,20 +2,19 @@ package org.faster.responsefiles;
 
 import java.io.OutputStream;
 
+import org.faster.feedback.FileFeedback;
 import org.faster.virtualpath.VirtualPath;
 
 public class CreatedSentDataDefault implements CreatedSentData {
 
 	@Override
 	public SentData directory(VirtualPath dir, OutputStream out) {
-		// TODO Auto-generated method stub
-		return null;
+		return new SdDefault(out, FileFeedback.NOTHING);
 	}
 
 	@Override
 	public SentData file(VirtualPath dir, OutputStream out) {
-		// TODO Auto-generated method stub
-		return null;
+		return new SdDefault(out, FileFeedback.NOTHING);
 	}
 
 }
