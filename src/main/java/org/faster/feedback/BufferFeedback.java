aadaac02de3322d11a7aba92c183dbf29328e6ce
eljames <eljames4@hotmail.com>
2018-10-22 06:04:40 -0300

    Comments changed

--- src/main/java/org/faster/feedback/BufferFeedback.java
+++ src/main/java/org/faster/feedback/BufferFeedback.java
@@ -4,7 +4,7 @@ public interface BufferFeedback {
 	
 	/**
 	 * This method says to a BufferFeedBack object that a amount of bytes {@code read} has been written.
-	 * For instance, it can be used to notify each piece of data already written to a file. For instance, if this method was called 5 times
+	 * For instance, it can be used to notify each piece of data already written to a user interface. For instance, if this method was called 5 times
 	 * , and each time {@code read} was 20, it means that was read 5 x 20 bytes = 100 bytes already read.
 	 */ 
 	void feed(int read);
