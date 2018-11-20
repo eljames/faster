f0f0c678e728d4d63afa42f6d1748dcdc6ad07a1
eljames <eljames4@hotmail.com>
2018-11-20 01:23:47 -0200

    Throws IOException

--- src/main/java/org/faster/responsefiles/CreatedSentData.java
+++ src/main/java/org/faster/responsefiles/CreatedSentData.java
@@ -1,11 +1,12 @@
 package org.faster.responsefiles;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 import org.faster.virtualpath.VirtualPath;
 
 public interface CreatedSentData {
 	
-	SentData directory(VirtualPath dir, OutputStream out);
-	SentData file(VirtualPath dir, OutputStream out);
+	SentData directory(VirtualPath dir, OutputStream out) throws IOException;
+	SentData file(VirtualPath dir, OutputStream out) throws IOException;
 }
